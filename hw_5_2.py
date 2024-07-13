3100abf23b87d33fdfc3614ea055179d628477c3
Sed-24 <81331707+Sed-24@users.noreply.github.com>
2024-07-13 15:55:15 +0300

    Add files via upload

--- hw_5_2.py
+++ hw_5_2.py
@@ -1,13 +1,16 @@
-
-
-def generator_numbers(text: str):
-    num = [float(n) for n in text.split()]
-    return print(num)
-
+import re
 
 text = ("Загальний дохід працівника складається з декількох частин: "
         "1000.01 як основний дохід, доповнений додатковими надходженнями "
         "27.45 і 324.00 доларів.")
 
 
-print(generator_numbers(text))
+def generator_numbers(text: str):
+    return [float(n) for n in text.split() if re.search(r'\d[\d.,]*', n)]
+
+
+def sum_profit(num):
+    return sum(num)
+
+
+print(sum_profit(generator_numbers(text)))
